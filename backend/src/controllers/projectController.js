6099360c8191d9c9e8c13134f374ac7621f427da
Divs-Git <divyansh.sri258@gmail.com>
2024-12-15 01:04:24 +0530

    added initial ui button to create new project

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -6,10 +6,6 @@ import uuid4 from 'uuid4'
 const execPromisified = util.promisify(child_process.exec)
 
 export const createProjectController = async (req, res) => {
-  //   const { stdout, stderr } = await execPromisified('ls')
-  //   console.log('stdout:', stdout)
-  //   console.error('stderr:', stderr)
-
   // Create a unique id and then inside the projects folder create a new folder with that id
   const projectId = uuid4()
   console.log('New project id is', projectId)
